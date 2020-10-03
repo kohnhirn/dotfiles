d0990c8d9e87196f243bc295ea48d739ec04b51d
kohnhirn <kohnhirn2014@gmail.com>
2020-10-03 21:42:16 +0900

    fix install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -5,7 +5,8 @@ mkdir -p $DOT_DIR
 
 curl -sSL https://github.com/kohnhirn/dotfiles/tarball/master | tar -xvz -C $DOT_DIR --strip-components 1
 
-for f in $DOT_DIR/.??*
+cd $DOT_DIR
+for f in .??*
 do
 	[[ `basename $f` == ".git" ]] && contiune
 	ln -fs $f $HOME/$f
